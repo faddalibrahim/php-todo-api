b0ec5201bb04ff3483e10ec0e250e54540f0026f
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-05-22 23:20:29 +0000

    comment utility functions

--- util/functions.util.php
+++ util/functions.util.php
@@ -1,24 +1,60 @@
 <?php
 
+/**
+ * fetches and converts into an associative array the json data sent from the client
+ * 
+ * @return array 
+ */
+
 function getPayload(){
     return (array) json_decode(file_get_contents("php://input"));
 }
+
+/**
+ * checks if incoming request type is GET
+ * 
+ * @return boolean true or false
+ */
 
 function isGetRequest() { 
     return $_SERVER[REQUEST_METHOD] === GET;
 }
 
+/**
+ * checks if incoming request type is POST
+ * 
+ * @return boolean true or false
+ */
+
 function isPostRequest() { 
     return $_SERVER[REQUEST_METHOD] === POST;
 }
+
+/**
+ * checks if incoming request type is PUT
+ * 
+ * @return boolean true or false
+ */
 
 function isPutRequest() { 
     return $_SERVER[REQUEST_METHOD] === PUT;
 }
 
+/**
+ * checks if incoming request type is DELETE
+ * 
+ * @return boolean true or false
+ */
+
 function isDeleteRequest() { 
     return $_SERVER[REQUEST_METHOD] === DELETE;
 }
+
+/**
+ * encodes ands sends response to the client
+ * 
+ * @param array { response } the response to be sent to the client
+ */
 
 function sendResponse($response){
     exit(json_encode($response));
