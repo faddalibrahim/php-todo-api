fc073a8af0cab205c0791c38f9a6cbea0d862ee1
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-05-26 03:46:02 +0000

    .env ready and working

--- config/credentials.config.php
+++ config/credentials.config.php
@@ -1,8 +1,13 @@
 <?php
 
-define("HOST", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
-define("DB_NAME", "todo");
+require_once(__DIR__.'/../vendor/autoload.php');
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
+define("HOST", $_ENV['HOST']);
+define("USERNAME", $_ENV['USERNAME']);
+define("PASSWORD", $_ENV['PASSWORD']);
+define("DB_NAME", $_ENV['DB_NAME']);
 
 ?>
