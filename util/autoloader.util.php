d75ea0233427952a8cb164b64bb551e9ff66865b
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-05-26 02:46:27 +0000

    check autoloader get file exists

--- util/autoloader.util.php
+++ util/autoloader.util.php
@@ -7,6 +7,8 @@ function myAutoloader($classname){
     $ext = ".model.php";
     $fullPath = __DIR__ . $path . $classname . $ext;
 
+    if(!file_exists($fullPath)) return false;
+
     include_once($fullPath);
 }
 
